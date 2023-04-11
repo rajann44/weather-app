fafc7adfa6d1d260fd8009df4565323a6d107d43
Rajan Chaudhary <rajann44@gmail.com>
2023-04-11 21:54:22 +0200

    custom bg on weather

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import { useState } from "react";
+import defaultBackground from "./assets/weather.jpg";
+import temp_below10 from "./assets/below10.jpg";
+import temp_between10_20 from "./assets/10_20.jpg";
+import temp_between20_30 from "./assets/20_30.jpg";
+import temp_between30_40 from "./assets/30_40.jpg";
+import temp_above_40 from "./assets/above_40.jpg";
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
+
+  const [background, setBackground] = useState(defaultBackground);
+
+  const checkWeatherAndChangeBG = (number) => {
+    switch (true) {
+      case number < 10:
+        setBackground(temp_below10);
+        break;
+      case number >= 10 && number < 20:
+        setBackground(temp_between10_20);
+        break;
+      case number >= 20 && number < 30:
+        setBackground(temp_between20_30);
+        break;
+      case number >= 30 && number < 40:
+        setBackground(temp_between30_40);
+        break;
+      case number >= 40 && number < 60:
+        setBackground(temp_above_40);
+        break;
+      default:
+        setBackground(defaultBackground);
+        break;
+    }
+  };
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
 
@@ -12,12 +43,18 @@ function App() {
       let jsonFormatted = await weatherResponse.json();
       console.log(jsonFormatted);
       setData(jsonFormatted);
+      checkWeatherAndChangeBG(jsonFormatted.main.temp.toFixed());
       setLocation("");
     }
   };
 
   return (
-    <div className="app">
+    <div
+      className="app"
+      style={{
+        background: `url(${background}) no-repeat center center/cover`,
+      }}
+    >
       <div className="search">
         <input
           value={location}
